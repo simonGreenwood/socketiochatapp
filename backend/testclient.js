7f9fb2c55297a9ac8bb925b42cc8838023e5d493
Simon <76248532+SimonGreenwood@users.noreply.github.com>
2022-02-22 21:52:30 +0000

    removed backend directory

--- backend/testclient.js
+++
@@ -1,4 +0,0 @@
-const io = require("socket.io-client")
-const socket = io('http://localhost:3001') 
-socket.on('message',message=>console.log(message))
-socket.emit('message','Message from testclient.js')
