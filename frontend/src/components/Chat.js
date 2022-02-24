f4313342151a74caa075eac22a1a82f58a67ff52
Simon <76248532+SimonGreenwood@users.noreply.github.com>
2022-02-24 21:52:22 +0000

    Created the main layout using flexbox

--- frontend/src/components/Chat.js
+++ frontend/src/components/Chat.js
@@ -1,28 +1,69 @@
 import React, {useState,useEffect} from "react"
-
-
-
-const Chat = ({messages,updateMessages,socket,username}) => {
+import Send from '../send.svg';
+const Chat = ({messages, updateMessages, updateOnline, online, socket, username}) => {
   const [newMessage,setNewMessage] = useState('')
   const msgUpdater = () => {
     socket.on('message',(from,msg) => {
       updateMessages({from,msg})
     })
+    socket.on('online update',(online) => {
+      console.log('Online:',online)
+      updateOnline(online)
+    })
     return()=>{
       socket.off('message')
     }
   }
+  
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    socket.emit('message',username,newMessage)
+    setNewMessage('')
+  }
+
+  const mainDiv = {
+    display:'flex',
+    flexFlow:'row wrap'
+  }
+  const onlineDiv = {
+    top:'0px',
+    flexBasis:'auto',
+    position:'sticky',
+    paddingLeft:'10px'
+  }
+
+  const messageDiv = {
+
+    wordWrap: 'break-word',
+    flexBasis: '75%',
+    marginBottom:'auto'
+   } 
+  const bottomBar = {
+    position:'fixed',
+    bottom:'0px',
+    width:'100%',
+    paddingBottom:'10px',
+    height:'5%',
+    background:'#383838',
+    display:'flex',
+    flexFlow:'row nowrap',
+    paddingTop:'15px',
+    paddingBottom:'15px'
+  } 
   useEffect(msgUpdater,[socket,messages,updateMessages])
   return (
-    <div>
-      {messages.map(msg=><p>{`${msg.from}: ${msg.msg}`}</p>)}
-      <form onSubmit={event => {
-        event.preventDefault()
-        socket.emit('message',username,newMessage)
-        setNewMessage('')
-      }}>
-        <input value={newMessage} onChange={event => setNewMessage(event.target.value)}/>
-        <button formAction="submit">SEND MSG</button>
+    <div style={mainDiv}>
+      <div style={messageDiv}>
+        <h1>Chat App</h1>
+        {messages.map(msg=><p style={{overflow:'hidden'}}>{`${msg.from}: ${msg.msg}`}</p>)}
+      </div>
+      <div style={onlineDiv}>
+        <h1>Online</h1>
+        {online.map(user => <p>{user.name}</p>)}
+      </div>
+      <form onSubmit={event => handleSubmit(event)} style={bottomBar}>
+          <input value={newMessage} onChange={event=>setNewMessage(event.target.value)} style={{}}/>
+          <button style={{backgroundImage:`url(${Send})`}}></button>
       </form>
     </div>
   )
--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -4,19 +4,22 @@ import Chat from "./Chat"
 const Main = ({username}) => {
   const [socket,setSocket] = useState(null)
   const [messages,setMessages] = useState([])
+  const [online,setOnline] = useState([])
   const updateMessages = (item) => {
     setMessages(messages.concat(item))
   }
+  const updateOnline = (onlineList) => {
+    setOnline(onlineList)
+  }
   const hook = () => {
     const sock = io('localhost:3001')
-    setSocket(sock)
     sock.emit('user connect',username)
+    setSocket(sock)
   }
   useEffect(hook,[])
   return (
     <div>
-    <h1>Chat App</h1>
-    {socket ? <Chat messages={messages} updateMessages={updateMessages} socket={socket} username={username}/>:<p>Loading...</p>}
+    {socket ? <Chat messages={messages} updateMessages={updateMessages} updateOnline={updateOnline}  online={online} socket={socket} username={username}/>:<p>Loading...</p>}
     </div>
   );
 }
